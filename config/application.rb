6f06ba99e09cb6be32303cefaaea6843c4249d89
Nicolas Dumont <nic_dumont@hotmail.com>
2013-09-11 18:27:48 +0200

    inscription user finie

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +19,10 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
   end
 end
 
