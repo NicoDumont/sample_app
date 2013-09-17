0df72b90496e9647a27500d72181f308fc6f9f9a
Nicolas Dumont <nic_dumont@hotmail.com>
2013-09-17 14:00:46 +0200

    Ajout du suivi des utilisateurs

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
+	
   attr_accessible :content
 
   belongs_to :user
@@ -7,5 +8,12 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence => true
 
   default_scope :order => 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    followed_ids = user.following.map(&:id).join(", ")
+    where("user_id IN (#{followed_ids}) OR user_id = ?", user)
+  end
+
+
 end
 
